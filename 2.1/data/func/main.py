cec593df13a089b57deaa79b5ee345639c17c91c
SlavaSlavyan <tortik2000.g@gmail.com>
2024-11-28 13:48:50 +0300

    2.1.5

--- 2.1/data/func/main.py
+++ 2.1/data/func/main.py
@@ -4,7 +4,7 @@ pygame.init()
 
 width, height = 1000, 800
 screen = pygame.display.set_mode((width, height),pygame.RESIZABLE)
-pygame.display.set_caption("Ultimate Tic Tac Toe 2.1.4 DEV")
+pygame.display.set_caption("Ultimate Tic Tac Toe 2.1.5 DEV")
 
 status = 0
 anim = 0
@@ -36,4 +36,34 @@ class Main:
     
     def click(x,y):
 
-        pass
+        global status,anim
+
+        if status == 0:
+            if anim >= 0 and anim < 1:
+                anim = 1
+            elif anim >= 1 and anim < 1.2:
+                anim = 1.2
+                status = 1
+        elif status == 1:
+            if anim == 1.2:
+                arg = Main.startscreenclick(x,y)
+                if arg != None:
+                    status = 2+arg
+                    anim = 1.3
+        
+    def startscreenclick(x,y):
+
+        x = width//2-x
+        y = height//2-y
+
+        selected_button = 0
+
+        for i in range(3):
+            if x < 200 and x > -200 and y < 0+50*i and y > -50+50*i:
+                return selected_button
+            
+            else:
+                selected_button += 1
+        
+        return None
+        
