9c38fcad85b8dff0c41fb04d18735f79e5f6f2a0
SlavaSlavyan <tortik2000.g@gmail.com>
2025-02-24 14:01:11 +0300

    test 2

--- 2.9/display/main.py
+++ 2.9/display/main.py
@@ -2,6 +2,7 @@ import pygame
 
 from display.game import Game
 from func.filereader import File
+from func.text import Text
 
 class Display: # Этот клас отвечает за условия выведения экранов игры (anim) по типу "startscreen" или "game"
 
@@ -9,12 +10,15 @@ class Display: # Этот клас отвечает за условия выве
         
         self.anim = 'game_start' # Отвечает за пока определённой "анимации" или экрана.
 
+        self.Text = Text(m)
         self.Game = Game(m)
         
 
     def main(self,m): # Основная функция, которая определяет условия вывода информации на экран
 
+        m.w.screen.fill((0,0,0))
+
         if self.anim[:4] == 'game':
             self.Game.main(m)
 
-        self.test(m)
+        self.Text.F3(m,[f"FPS: {m.w.fps}"])
--- 2.9/func/text.py
+++ 2.9/func/text.py
@@ -7,23 +7,48 @@ class Text:
     def __init__(self,m):
         
         self.letters = File.load('assets\\letters')
+    
+    def symbol(self,m,symbol,pos,size):
 
-    def letter(self,m,pos,letter):
+        symbol_load = self.letters[symbol]
+        symbol_map = symbol_load['map']
+        symbol_size = symbol_load['size']
 
-        pass
+        z = round(size/2,1)
+        x = pos[0]
+        y = pos[1] - symbol_size[1]*size
 
-    def string(self,m,string,args):
+        for Y in range(len(symbol_map)):
 
-        z = round(args['size']/2,1)
-        x = args['pos'][0]
-        y = args['pos'][1]
+            for X in range(len(symbol_map[0])):
 
-        for letter in string:
-            
-            self.letter(m,(x,y),letter)
-            x += len(self.letters[letter][0])
+                if symbol_map[Y][X] == 1:
+
+                    pos = [[x+0+2*z*X,y+0+2*z*Y],[x+(z*2-1)+2*z*X,y+0+2*z*Y],[x+(z*2-1)+2*z*X,y+(z*2-1)+2*z*Y],[x+0+2*z*X,y+(z*2-1)+2*z*Y]]
+                    pygame.draw.polygon(m.w.screen, (255,255,255), pos)
+        
+        return symbol_size[0]*size
 
 
+    def F3(self,m,text):
+
+        size = 2
+        y = 6*size
+
+        if isinstance(text,list):
+
+            for line in text:
+
+                x = 1*size
+                for s in line:
+
+                    if s == '"':
+                        s = "''"
+
+                    offset = self.symbol(m,s.upper(),[x,y],size)
+                    x = x + 1*size + offset
+
+                y += 7*size
 
     def test(self,m):
 
