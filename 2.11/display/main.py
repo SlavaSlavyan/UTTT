215f9d1032ec9fd396fd068595d932469667033a
Svyatoslav <tortik2000.g@gmail.com>
2025-03-26 12:57:10 +0300

    2.11.2

---
+++ 2.11/display/main.py
@@ -0,0 +1,54 @@
+import pygame
+
+from display.logo import Logo
+
+# Класс отвечающий за визуальный вывод
+class Display:
+    
+    def __init__(self,m):
+        
+        m.log.write('[DEBUG] Инициализация класса Display')
+        
+        
+        self.colors = m.Manager.load(m,f'data\\them\\{m.config['them']}')
+        
+        if self.colors[1]:
+            self.colors = self.colors[0]
+        else:
+            m.log.write('[FATAL] Не удалось импортировать палитру!')
+            m.end()
+        
+        # Эта переменная очень похожа на status, но отвечает не за ввод информации, а за вывод.
+        self.anim = 'logo_start'
+        
+        self.width, self.height = m.config['start-screen-size']
+        self.setscreen(m)
+        
+        self.Logo = Logo(m,self)
+            
+        self.clock = pygame.time.Clock() # класс для последовательного отображения кадров без задержек или спешки (контроль FPS)
+        self.fps = self.clock.get_fps()
+    
+    # Основная функция класса, которая даёт условия для вывода инофрмации
+    def main(self,m):
+        
+        self.width, self.height = self.screen.get_size()
+        
+        if self.anim[:4] == 'logo':
+            self.Logo.main(m)
+    
+    def setscreen(self,m): # Обновление информации экрана
+        
+        m.log.write('[DEBUG] Изменение режима окна.')
+        
+        if m.config['fullscreen']:
+
+            m.log.write('[INFO] Режим окна был изменён на полноэкранный.')
+            self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN | pygame.DOUBLEBUF)
+        
+        else:
+            
+            m.log.write('[INFO] Режим окна был изменён на оконный.')
+            self.screen = pygame.display.set_mode((self.width,self.height),pygame.RESIZABLE | pygame.DOUBLEBUF)
+
+        m.saveconfig(False)
---
+++ 2.11/func/PlayerInput.py
@@ -0,0 +1,18 @@
+import pygame
+
+# Класс обработки ввода от игрока
+class PlayerInput:
+    
+    def __init__(self,m):
+        
+        m.log.write('[DEBUG] Инициализация класса PlayerInput')
+        
+    def main(self,m):
+        
+        for event in pygame.event.get(): # проверка всех эвентов в pygame
+            
+            if event.type == pygame.QUIT: # выход из игры
+                
+                m.log.write('[DEBUG] Запуск инструкций для отключения программы')
+                
+                m.end() # запуск функции для выхода из игры
